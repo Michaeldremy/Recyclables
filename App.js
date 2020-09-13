b7bb84ae7ae97ee507f578975fb270949df6e73c
Michaeldremy <michaeldremy@gmail.com>
2020-09-13 15:49:55 -0700

    Working on the messages screen

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import React from "react";
 import { View } from "react-native";
 import Card from "./app/components/Card";
 import ListDetailsScreen from "./app/screens/ListDetailsScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
 
 export default function App() {
   return (
-    <ListDetailsScreen/>
+    <MessagesScreen/>
   );
 }
