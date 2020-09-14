bcdb71ca71336c2a80d1fc702605266ac49685f8
Michaeldremy <michaeldremy@gmail.com>
2020-09-14 12:39:58 -0700

    Added a screen for listings

--- App.js
+++ App.js
@@ -6,9 +6,10 @@ import MessagesScreen from "./app/screens/MessagesScreen";
 import Screen from "./app/components/Screen";
 import Icon from "./app/components/Icon";
 import AccountScreen from "./app/screens/AccountScreen";
+import ListingsScreen from "./app/screens/ListingsScreen";
 
 export default function App() {
   return (
-    <AccountScreen />
+    <ListingsScreen />
   );
 }
