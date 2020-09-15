fb18bde05da5a820e4ae301445572dcccb1f51b2
Michaeldremy <michaeldremy@gmail.com>
2020-09-14 18:08:40 -0700

    Updated AppPicker

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { View } from "react-native";
 import Card from "./app/components/Card";
 import ListDetailsScreen from "./app/screens/ListDetailsScreen";
@@ -11,11 +11,25 @@ import AppTextInput from "./app/components/AppTextInput";
 import AppPicker from "./app/components/AppPicker";
 import AppText from "./app/components/AppText";
 
+const catergories = [
+  { label: "Furniture", value: 1 },
+  { label: "Gaming", value: 2 },
+  { label: "Cookware", value: 3 },
+];
+
 export default function App() {
+  const [category, setCategory] = useState(catergories[0]);
+
   return (
     <Screen>
-      <AppPicker icon="apps" placeholder="Category" />
-      <AppTextInput icon="email" placeholder="email"/>
+      <AppPicker
+        selectedItem={category}
+        onSelectItem={(item) => setCategory(item)}
+        items={catergories}
+        icon="apps"
+        placeholder="Category"
+      />
+      <AppTextInput icon="email" placeholder="email" />
     </Screen>
   );
 }
