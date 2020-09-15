f8b15d48be4a1c3e73d7004a4f94d32e00d6dede
Michaeldremy <michaeldremy@gmail.com>
2020-09-15 12:11:05 -0700

    Improvements for the following components: AppFormField, AppFormPicker, AppPicker, AppText, AppTextInput, Card, ListItem, ListingEditScreen, and MessagesScreen

--- app/components/Card.js
+++ app/components/Card.js
@@ -2,15 +2,19 @@ import React from "react";
 import { View, StyleSheet, Image } from "react-native";
 
 import AppText from "./AppText";
-import color from '../config/color';
+import color from "../config/color";
 
 function Card({ title, subTitle, image }) {
   return (
     <View style={styles.card}>
       <Image style={styles.image} source={image} />
       <View style={styles.detailsContainer}>
-        <AppText style={styles.title}>{title}</AppText>
-        <AppText style={styles.subTitle}>{subTitle}</AppText>
+        <AppText style={styles.title} numberOfLines={1}>
+          {title}
+        </AppText>
+        <AppText style={styles.subTitle} numberOfLines={1}>
+          {subTitle}
+        </AppText>
       </View>
     </View>
   );
--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { View, StyleSheet, Image, TouchableHighlight } from "react-native";
-
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import Swipeable from "react-native-gesture-handler/Swipeable";
+
 import AppText from "./AppText";
 import color from "../config/color";
 
-function ListItem({ title, subTitle, image, IconComponent, onPress, renderRightActions }) {
+function ListItem({
+  title,
+  subTitle,
+  image,
+  IconComponent,
+  onPress,
+  renderRightActions,
+}) {
   return (
     <Swipeable renderRightActions={renderRightActions}>
       <TouchableHighlight underlayColor={color.light} onPress={onPress}>
@@ -13,9 +21,16 @@ function ListItem({ title, subTitle, image, IconComponent, onPress, renderRightA
           {IconComponent}
           {image && <Image style={styles.image} source={image} />}
           <View style={styles.detailsContainer}>
-            <AppText style={styles.title}>{title}</AppText>
-            {subTitle && <AppText style={styles.subTitle}>{subTitle}</AppText>}
+            <AppText style={styles.title} numberOfLines={1}>
+              {title}
+            </AppText>
+            {subTitle && <AppText style={styles.subTitle} numberOfLines={2}>{subTitle}</AppText>}
           </View>
+          <MaterialCommunityIcons
+            name="chevron-right"
+            size={25}
+            color={color.medium}
+          />
         </View>
       </TouchableHighlight>
     </Swipeable>
@@ -26,11 +41,13 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
     padding: 15,
-    backgroundColor: color.white
+    backgroundColor: color.white,
+    alignItems: "center",
   },
-  detailsContainer:{
+  detailsContainer: {
+    flex: 1,
     marginLeft: 10,
-    justifyContent: 'center'
+    justifyContent: "center",
   },
   image: {
     width: 70,
