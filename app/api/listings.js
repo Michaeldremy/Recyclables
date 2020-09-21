efc17c9132aaaa7d5a271f2d4c8c5183a693ae1e
Michaeldremy <michaeldremy@gmail.com>
2020-09-21 09:12:41 -0700

    Extending Listings API

--- app/api/listings.js
+++ app/api/listings.js
@@ -1,9 +1,31 @@
-import client from './client'
+import client from "./client";
 
-const endpoint ="/listings"
+const endpoint = "/listings";
 
-const getListings = () => client.get(endpoint)
+const getListings = () => client.get(endpoint);
+
+const addListing = (listing) => {
+  const data = new FormData();
+  data.append("title", listing.title);
+  data.append("price", title.price);
+  data.append("categoryId", title.category.value);
+  data.append("description", title.description);
+
+  listing.images.forEach(image, (index) => {
+    data.append("images", {
+      name: "image" + index,
+      type: "image/jpeg",
+      uri: image,
+    });
+  });
+
+  if (listing.location)
+    data.append("location", JSON.stringify(listing.location));
+
+  return client.post(endpoint, data);
+};
 
 export default {
+  addListing,
   getListings,
-}
+};
